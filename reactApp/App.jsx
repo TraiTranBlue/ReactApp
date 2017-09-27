98c231aec8d0bf09010eef22ccabca6eb7221c30
traitran <traitv@vng.com.vn>
2017-09-27 18:06:43 +0700

    create source

---
+++ reactApp/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./client/component/Header.jsx";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <h1>huhuhuhu</h1>
+            </div>
+
+        );
+    }
+}
+
+export default App;
---
+++ reactApp/client/component/Header.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+
+export default class Header extends Component {
+    render(){
+        render()
+        {
+            return (
+                <div>
+                    <header>
+                        <img src="client/images/img_logo.jpg" className="img-logo" />
+                        <img src="client/images/banner_1.jpg" className="img-banner" />
+                    </header>
+                </div>
+            );
+        }
+    }
+}
+
